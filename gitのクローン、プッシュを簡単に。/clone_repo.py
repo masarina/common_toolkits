31e61e5ec3c898d5879f4adc437c2ce5ed298149
masarina <50221377+masarina@users.noreply.github.com>
2024-08-17 19:20:31 +0900

    Update clone_repo.py

--- gitのクローン、プッシュを簡単に。/clone_repo.py
+++ gitのクローン、プッシュを簡単に。/clone_repo.py
@@ -11,24 +11,24 @@ subprocess.run(["git", "config", "--global", "user.name", git_user])
 subprocess.run(["git", "config", "--global", "user.email", git_email])
 
 # リポジトリのURLを入力
-repo_url = input("リポジトリのURLを入力してください (例: https://github.com/user/repo.git か git@github.com:user/repo.git): ")
+repo_url = input("リポジトリのURLを入力してください (例: https://github.com/user/repo.git または git@github.com:user/repo.git): ")
 
 # HTTPS URLをSSH URLに変換
 if repo_url.startswith("https://github.com/"):
     ssh_url = repo_url.replace("https://github.com/", "git@github.com:")
-    print(f"HTTPS URLをSSH URLに変換しました: {ssh_url}")
-    repo_url = ssh_url
+else:
+    ssh_url = repo_url
 
 # クローン先のディレクトリ名を決定（リポジトリ名を利用）
-repo_name = os.path.basename(repo_url).replace(".git", "")
+repo_name = os.path.basename(ssh_url).replace(".git", "")
 if not os.path.exists(repo_name):
     os.mkdir(repo_name)
 
 # リポジトリ名とURLをpickleで保存
 with open('repo_info.pkl', 'wb') as f:
-    pickle.dump({'repo_name': repo_name, 'repo_url': repo_url}, f)
+    pickle.dump({'repo_name': repo_name, 'repo_url': ssh_url}, f)
 
 # SSH認証でのクローンを実行
-subprocess.run(["git", "clone", repo_url, repo_name])
+subprocess.run(["git", "clone", ssh_url, repo_name])
 
 print(f"リポジトリが {repo_name} にクローンされました。")
