9909f94a7d01955963d1f9d2961ce3a7e9c5278b
Masarina <50221377+masarina@users.noreply.github.com>
2024-09-06 01:48:28 +0900

    KLoadToBallPlayer_By_ChannelType.py を更新

---
+++ Archive/DiscordBot/Players/Player_ChannelType/LoadToBallPlayer_By_ChannelTypeDir/KLoadToBallPlayer_By_ChannelType.py
@@ -0,0 +1,20 @@
+import os
+from Players_CommonPlayers.SuperPlayerDir.SuperPlayer import SuperPlayer
+
+class LoadToBallPlayer_By_BallUpdaters(SuperPlayer):
+    def __init__(self):
+        super().__init__()  # スーパークラスの初期化メソッドを呼び出す
+        self.my_name = None
+
+    def return_my_name(self):
+        return "LoadToBallPlayer_By_BallUpdaters"
+
+    def main(self):
+        """
+        JSONファイルのパスをball.all_data_dictに追加するプレイヤー。
+        """
+        
+        # ball.all_data_dictにパスを追加
+        self.one_time_world_instance.ball.all_data_dict["all_user_information_dict"] = one_time_world_instance.updateUIDPlayer.save_path
+
+        return "Completed"
