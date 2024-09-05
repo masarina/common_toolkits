68cfd2d84643c87e64bbfade33e52b00549101fd
Masarina <50221377+masarina@users.noreply.github.com>
2024-09-06 03:43:43 +0900

    BallObject.py を更新

--- Archive/DiscordBot/Players/BallObject.py
+++ Archive/DiscordBot/Players/BallObject.py
@@ -1,8 +1,6 @@
 import copy
 import sys 
 sys.path.append('/etc/api_store')
-from ALPHAVANTAGE_API import ALPHAVANTAGE_API 
-from ALPACA_API import ALPACA_API
 
 class BallObject:
     """
@@ -28,26 +26,6 @@ class BallObject:
         # 全ての情報を格納する辞書の用意
         self.all_data_dict = {}
         
-        # インスタンスの用意
-        aLPHAVANTAGE_API = ALPHAVANTAGE_API()
-        aLPACA_API = ALPACA_API()
-        
-        # apiの取得
-        aLPHAVANTAGE_API_OPEN_KEY = aLPHAVANTAGE_API.API_OPEN_KEY # アルファバンテージ
-        aLPHAVANTAGE_API_QUERY_URL = aLPHAVANTAGE_API.QUERY_URL # アルファバンテージ
-        aLPACA_API_OPEN_KEY = aLPACA_API.API_OPEN_KEY # アルパカ
-        aLPACA_API_SECRET_KEY = aLPACA_API.API_SECRET_KEY # アルパカ
-        aLPACA_API_BASE_URL = aLPACA_API.BASE_URL # アルパカ
-        self.all_data_dict.update(
-            {
-            "aLPHAVANTAGE_API_OPEN_KEY":aLPHAVANTAGE_API_OPEN_KEY,
-            "aLPHAVANTAGE_API_QUERY_URL":aLPHAVANTAGE_API_QUERY_URL,
-            "aLPACA_API_OPEN_KEY":aLPACA_API_OPEN_KEY,
-            "aLPACA_API_SECRET_KEY":aLPACA_API_SECRET_KEY,
-            "aLPACA_API_BASE_URL":aLPACA_API_BASE_URL
-            }
-        ) 
-        
         
         """
         デバッグ用（メンバ変数は全てDebugPlayerがデバッグしてくれます。）
@@ -65,42 +43,7 @@ class BallObject:
         elif self.schedule_mode == 'Mode_DataDownload':
             self.schedule_mode = 'Mode_TrainingDataFormat'
             
-        # 学習開始
-        elif self.schedule_mode == 'Mode_TrainingDataFormat':
-            self.schedule_mode = "Mode_Training"
 
-        # 推論結果を保持する変数の用意
-        elif self.schedule_mode == 'Mode_Training':
-            self.schedule_mode = "Mode_ResetPredictedLog"
-
-        # 推論用データを「ダウンロード、フォーマット、推論」までを実施。
-        elif self.schedule_mode == 'Mode_ResetPredictedLog':
-            self.schedule_mode = "Mode_DataFormat_And_Prediction_AllCompany"
-            
-        # 全ての銘柄の「ダウンロード、フォーマット、推論」が完了していない場合、このモードを繰り返す。
-        elif self.schedule_mode == 'Mode_DataFormat_And_Prediction_AllCompany' and world.ball.all_data_dict["all_predictions_complete"] != True:
-            self.schedule_mode = "Mode_DataFormat_And_Prediction_AllCompany"
-
-        # 全ての銘柄の推論が完了した場合、トレード結果を保持する変数の用意
-        elif self.schedule_mode == 'Mode_DataFormat_And_Prediction_AllCompany' and world.ball.all_data_dict["all_predictions_complete"] == True:
-            self.schedule_mode = "Mode_ResetTradedLog"
-
-        # トレード開始
-        elif self.schedule_mode == 'Mode_ResetTradedLog':
-            self.schedule_mode = "Mode_Trading_AllCompany"
-
-        # 全ての銘柄のトレードが完了していない場合、このモードを繰り返す。
-        elif self.schedule_mode == 'Mode_Trading_AllCompany' and world.ball.all_data_dict['all_Trade_complete'] != True:
-            self.schedule_mode = 'Mode_Trading_AllCompany'
-        
-        # 全ての銘柄のトレードが完了した場合、UTC時間23:59まで待機する。
-        elif self.schedule_mode == 'Mode_Trading_AllCompany' and world.ball.all_data_dict['all_Trade_complete'] == True:
-            self.schedule_mode = 'Mode_WeightAndStart_UTC2359'
-
-        # 待機が完了した場合、再度「推論 〜 トレード」までを実施する
-        elif self.schedule_mode == 'Mode_WeightAndStart_UTC2359':
-            self.schedule_mode = "Mode_ResetPredictedLog"
-            
         else:
             print(f"{self.schedule_mode} というスケジュールモードは存在しません。BallObjectの「schedule_mode_settings」と「schedule_dict」を確認してください。")
             exit(1)
@@ -136,100 +79,6 @@ class BallObject:
                 [['DownloadTrainingDataPlayer']],
                 [['FinalPlayer']]  # 例: 第二ミニスケジュール
             ]
-
-        elif mode_name == "Mode_TrainingDataFormat":
-            array_2d = [
-                [['FirstPlayer']],  # 例: 第一ミニスケジュール
-                [['NormalizeByPreviousDayAs100PercentPlayer']],
-                [['AppendWinLosePlayer']],
-                [['AppendNextDayInfoPlayer']],
-                [['NormalizeClosePctPlayer']],
-                [['DropColumnsPlayer']],
-                [['TimeSeriesFormatterPlayer']],
-                [['OverlapDataPlayer']],
-                [['TransformDataPlayer']],
-                [['RemoveNoneRowsPlayer']],
-                [['AddPrefixPlayer']],
-                [['AddSpecialTokensPlayer']],
-                [['SplitDataPlayer']],
-                [['ConvertCsvToTxtPlayer']],
-                [['SaveBallToPicklePlayer']],
-                [['FinalPlayer']]
-            ]
-
-
-        elif mode_name == "Mode_Training":
-            array_2d = [
-                [['FirstPlayer']],  # 例: 第一ミニスケジュール
-                [['LoadPickleToBallPlayer']],
-                [['SetupGpt2Player']],
-                [['SetupTokenizerPlayer']],
-                [['TrainingPlayer'],['SaveTrainedBallToPicklePlayer']],
-                [['FinalPlayer']]  # 例: 第nミニスケジュール
-            ]
-
-        elif mode_name == "Mode_ResetPredictedLog":
-            array_2d = [
-                [['FirstPlayer']],  # 例: 第一ミニスケジュール
-                [['ResetPredictedLogPlayer']],  # 全ての銘柄の推論結果がdict型で格納される。それを全てNoneにするプレイヤー。
-                [['CheckAllPredictionsCompletePlayer']], # 全ての銘柄の推論完了でTrueにするflagの、リセット。
-                [['FinalPlayer']]  # 例: 第二ミニスケジュール
-            ]
-
-        elif mode_name == "Mode_DataFormat_And_Prediction_AllCompany":
-            array_2d = [
-                [['FirstPlayer']],  # 例: 第一ミニスケジュール
-                
-                [['SelectNextCurrencyPlayer']],  # 推論する銘柄を1つ選択し、ballに保存。
-                [['DownloadTwoMonthsDataPlayer']], # 銘柄のデータをダウンロード
-                [['ExecuteNormalizationPlayer']], # 銘柄1つ分のデータのフォーマット開始
-                [['ExecuteAppendWinLosePlayer']],
-                [['ExecuteAppendNextDayInfoPlayer']],
-                [['ExecuteNormalizeClosePctPlayer']],
-                [['ExecuteDropColumnsPlayer']],
-                [['ExecuteTimeSeriesFormatterPlayer']],
-                [['ExecuteOverlapDataPlayer']],
-                [['ExecuteTransformDataPlayer']],
-                [['ExecuteAddPrefixPlayer']],
-                [['AddTopSpecialTokenPlayer']],
-                [['CsvToSpaceSeparatedTxtPlayer']],
-                [['RemoveRightmostColumnTxtPlayer']], # 銘柄1つ分のデータのフォーマット完了
-                
-                [['InferencePlayer']], # この銘柄を推論
-            
-                [['UpdatePredictedLogPlayer']], # 銘柄1つ、推論が完了したので、結果をballに記録する。
-                [['CheckAllPredictionsCompletePlayer']], #全ての銘柄の推論が完了していれば、all_predictions_complete に True を代入します。
-                
-                [['FinalPlayer']]
-            ]
-            
-            
-
-        elif mode_name == "Mode_ResetTradedLog":
-            array_2d = [
-                [['FirstPlayer']],  # 例: 第一ミニスケジュール
-                [['ResetTradedLogPlayer']], # トレード結果を記録する辞書のリセット
-                [['TradeFlagMakePlayer']], # 全ての銘柄のトレードが完了した時、all_Trade_complete を True とするが、そのall_Trade_completeの変数をballに用意するプレイヤー。
-                [['FinalPlayer']]
-            ]
-            
-        elif mode_name == "Mode_Trading_AllCompany":
-            array_2d = [
-                [['FirstPlayer']],  # 例: 第一ミニスケジュール
-                [['SelectNextTradeCurrencyPlayer']], # トレードする銘柄名を、1つ抽出。
-                [['TradeExecutionPlayer']], # この銘柄を、推論結果を元に、トレードする。
-                [['CheckAllTradesCompletePlayer']], # 全ての銘柄のトレードが完了したとき、all_Trade_complete に True を代入するプレイヤー。
-                [['FinalPlayer']]
-            ]
-            
-            
-        elif mode_name == "Mode_WeightAndStart_UTC2359":
-            array_2d = [
-                [['FirstPlayer']],  # 例: 第一ミニスケジュール
-                [['WaitForTimeZonePlayer']], # 無限whileループに入る。UTC時間23:59になった時、whileを抜け出すプレイヤー。
-                [['FinalPlayer']]
-            ]
-            
             
 
         elif mode_name == "end":
