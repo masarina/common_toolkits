dce0daebad2737a81514b542953021d397001c75
Masarina <50221377+masarina@users.noreply.github.com>
2024-09-11 06:31:50 +0900

    LoadToBallPlayer_By_MessageType.py を更新

--- Archive/DiscordBot/Players/Players_SendMessager/_LoadToBallPlayer_By_MessageType/LoadToBallPlayer_By_MessageType.py
+++ Archive/DiscordBot/Players/Players_SendMessager/_LoadToBallPlayer_By_MessageType/LoadToBallPlayer_By_MessageType.py
@@ -1,4 +1,5 @@
 import os
+import ModelInference
 from Players_CommonPlayers.SuperPlayerDir.SuperPlayer import SuperPlayer
 
 class LoadToBallPlayer_By_MessageType(SuperPlayer):
@@ -19,5 +20,10 @@ class LoadToBallPlayer_By_MessageType(SuperPlayer):
         
         # カテゴリー【プロジェクト】のカテゴリID
         self.one_time_world_instance.ball.all_data_dict["categoryID_of_ProjectCategory"] = 1195503636453793792
+        
+        # 推論モデルのインスタンス化を登録
+        modelInference = ModelInference.ModelInference()
+        self.one_time_world_instance.ball.all_data_dict["modelInference"] = modelInference
+        
 
         return "Completed"
