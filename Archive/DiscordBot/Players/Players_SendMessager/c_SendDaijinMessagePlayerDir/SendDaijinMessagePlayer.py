2b7444784f3ec497075bfc330bccc59fd38c131d
Masarina <50221377+masarina@users.noreply.github.com>
2024-09-11 06:41:48 +0900

    SendDaijinMessagePlayer.py を更新

--- Archive/DiscordBot/Players/Players_SendMessager/c_SendDaijinMessagePlayerDir/SendDaijinMessagePlayer.py
+++ Archive/DiscordBot/Players/Players_SendMessager/c_SendDaijinMessagePlayerDir/SendDaijinMessagePlayer.py
@@ -1,5 +1,6 @@
 import torch
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
+import ModelInference.ModelInference
 from Players_CommonPlayers.SuperPlayerDir.SuperPlayer import SuperPlayer
 
 class SendDaijinMessagePlayer(SuperPlayer):
@@ -14,5 +15,14 @@ class SendDaijinMessagePlayer(SuperPlayer):
         """
         を使って会話をするためのプレイヤー。
         """       
+        # 初期化
+        all_data_dict = self.one_time_world_instance.ball.all_data_dict
+        
+        # モデルツールの用意
+        if all_data_dict["modelInference"] == None:
+            all_data_dict["modelInference"] = ModelInference()
+        modelInference = all_data_dict["modelInference"]
+        
+        
 
         return "Completed"
