49c752d5a377059b79d55f8e11623dfc4d0a1a39
Masarina <50221377+masarina@users.noreply.github.com>
2024-09-10 23:00:36 +0900

    LoadToBallPlayer_By_MessageType.py を更新

---
+++ Archive/DiscordBot/Players/Players_SendMessager/_LoadToBallPlayer_By_MessageType/LoadToBallPlayer_By_MessageType.py
@@ -0,0 +1,20 @@
+import os
+from Players_CommonPlayers.SuperPlayerDir.SuperPlayer import SuperPlayer
+
+class LoadToBallPlayer_By_MessageType(SuperPlayer):
+    def __init__(self):
+        super().__init__()  # スーパークラスの初期化メソッドを呼び出す
+        self.my_name = None
+
+    def return_my_name(self):
+        return "LoadToBallPlayer_By_MessageType"
+
+    def main(self):
+        """
+        JSONファイルのパスをball.all_data_dictに追加するプレイヤー。
+        """
+        
+        # ball.all_data_dictにパスを追加
+        self.one_time_world_instance.ball.all_data_dict["all_user_information_dict"] = one_time_world_instance.updateUIDPlayer.save_path
+
+        return "Completed"
