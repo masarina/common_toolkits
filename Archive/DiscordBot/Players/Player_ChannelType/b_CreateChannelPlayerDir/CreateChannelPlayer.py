69b73f56a6b93204a1e52b69e50b8a552116b4e2
Masarina <50221377+masarina@users.noreply.github.com>
2024-09-11 04:00:16 +0900

    CreateChannelPlayer.py を更新

--- Archive/DiscordBot/Players/Player_ChannelType/b_CreateChannelPlayerDir/CreateChannelPlayer.py
+++ Archive/DiscordBot/Players/Player_ChannelType/b_CreateChannelPlayerDir/CreateChannelPlayer.py
@@ -29,16 +29,16 @@ class CreateChannelPlayer(SuperPlayer):
         メインメソッド。非同期のcreate_channelを実行する。
         """
         
-        # 作成するチャンネルについての設定
+        # 作成するチャンネルについての設定(一度に複数のチャンネルを設定出来ます。)
         # 1つ前のプレイヤーで設定してください
+        category_channel_2dList = self.one_time_world_instance.ball.all_data_dict["categoryID_and_channelID_2dList_of_create_Channel"]
         
-        self.one_time_world_instance.ball.all_data_dict["categoryID_and_channelID_2dList_of_create_Channel"]
         
-        
-        category_id = self.one_time_world_instance.ball.all_data_dict["category_id"]  # カテゴリIDを取得
-        channel_name = self.one_time_world_instance.ball.all_data_dict["channel_name"]  # チャンネル名を取得
+        # チャンネルの作成
         discord_client = self.one_time_world_instance.ball.all_data_dict["bot_instance"]  # botインスタンスを取得
-        
-        discord_client.loop.run_until_complete(self.create_channel(category_id, channel_name))  # 非同期メソッドを同期的に実行
+        for category_channel in category_channel_2dList:
+            category_id = category_channel[0] # カテゴリIDを取得
+            channel_name = category_channel[1] # チャンネル名を取得
+            discord_client.loop.run_until_complete(self.create_channel(category_id, channel_name))  # 非同期メソッドを同期的に実行
 
         return "Completed"
