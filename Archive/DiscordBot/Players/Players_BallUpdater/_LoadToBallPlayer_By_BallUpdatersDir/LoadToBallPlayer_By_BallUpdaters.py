bced28b6b0a262259f1748047ce9d3ebfc24d3e6
Masarina <50221377+masarina@users.noreply.github.com>
2024-09-12 05:26:57 +0900

    LoadToBallPlayer_By_BallUpdaters.py を更新

--- Archive/DiscordBot/Players/Players_BallUpdater/_LoadToBallPlayer_By_BallUpdatersDir/LoadToBallPlayer_By_BallUpdaters.py
+++ Archive/DiscordBot/Players/Players_BallUpdater/_LoadToBallPlayer_By_BallUpdatersDir/LoadToBallPlayer_By_BallUpdaters.py
@@ -1,4 +1,5 @@
 import os
+import json
 from Players_CommonPlayers.SuperPlayerDir.SuperPlayer import SuperPlayer
 
 class LoadToBallPlayer_By_BallUpdaters(SuperPlayer):
@@ -16,5 +17,31 @@ class LoadToBallPlayer_By_BallUpdaters(SuperPlayer):
         
         # ball.all_data_dictにパスを追加
         self.one_time_world_instance.ball.all_data_dict["all_user_information_dict"] = one_time_world_instance.updateUIDPlayer.save_path
+        
+        """ all_user_information_dictの チャンネルID-UIDのペア情報 の更新 """
+        # チャンネルと、チャンネル設立者の辞書のパスを取得
+        created_channelID_and_userID_json_path = self.one_time_world_instance.channelCreatorCheckerPlayer.channel_creator_info_json_path
+        
 
         return "Completed"
+        
+    def update_channelIDInfo(self,all_user_dict_json_path, channel_info_dict_json_path):
+        # データを取得
+        with open(all_user_dict_json_path, 'r') as json_file:
+            all_user_dict = json.load(json_file)
+        with open(channel_info_dict_json_path, 'r') as json_file:
+            channel_info_dict = json.load(json_file)
+            
+        # all_user_dictをアップデート
+        for ChID, UID in channel_info_dict:
+            channel_list = all_user_dict[str(UID)]["having_channel"]
+            channel_list += ChID
+            all_user_dict[str(UID)]["having_channel"] = channel_list
+        
+        
+        
+        
+        
+        
+        
+        
