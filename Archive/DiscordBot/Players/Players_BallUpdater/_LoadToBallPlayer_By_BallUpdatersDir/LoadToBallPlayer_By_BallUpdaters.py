7de786c723bece66c0ef0de12290296474c9b84a
Masarina <50221377+masarina@users.noreply.github.com>
2024-09-12 22:07:26 +0900

    LoadToBallPlayer_By_BallUpdaters.py を更新

--- Archive/DiscordBot/Players/Players_BallUpdater/_LoadToBallPlayer_By_BallUpdatersDir/LoadToBallPlayer_By_BallUpdaters.py
+++ Archive/DiscordBot/Players/Players_BallUpdater/_LoadToBallPlayer_By_BallUpdatersDir/LoadToBallPlayer_By_BallUpdaters.py
@@ -32,11 +32,18 @@ class LoadToBallPlayer_By_BallUpdaters(SuperPlayer):
         with open(channel_info_dict_json_path, 'r') as json_file:
             channel_info_dict = json.load(json_file)
             
-        # all_user_dictをアップデート
+        """ all_user_dictをアップデート """
+        key_name = "having_channel"
+        
+        # まだ having_channel が未定義であれば、からのリストを作成
+        for _, user_info_dict in all_user_dict.items()
+            user_info_dict.setdefault(key_name, []) # keyが未定義の場合[]で初期化
+        
+        # all_user_dictを更新
         for ChID, UID in channel_info_dict:
-            channel_list = all_user_dict[str(UID)]["having_channel"]
+            channel_list = all_user_dict[str(UID)][key_name]
             channel_list += ChID
-            all_user_dict[str(UID)]["having_channel"] = channel_list
+            all_user_dict[str(UID)][key_name] = channel_list
         
         
         
