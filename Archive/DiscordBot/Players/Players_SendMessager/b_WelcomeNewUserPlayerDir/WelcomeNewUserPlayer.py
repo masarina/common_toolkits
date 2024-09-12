770a58ffb6b39ed82e2223e7864c09b69664f492
Masarina <50221377+masarina@users.noreply.github.com>
2024-09-13 01:39:08 +0900

    WelcomeNewUserPlayer.py を更新

--- Archive/DiscordBot/Players/Players_SendMessager/b_WelcomeNewUserPlayerDir/WelcomeNewUserPlayer.py
+++ Archive/DiscordBot/Players/Players_SendMessager/b_WelcomeNewUserPlayerDir/WelcomeNewUserPlayer.py
@@ -51,8 +51,8 @@ class WelcomeNewUserPlayer(SuperPlayer):
         categoryID = one_time_world_instance.ball.all_data_dict["categoryID_of_ProjectCategory"]
         visitorUser_name = (await bot.fetch_user(target_user_id)).name
         self.one_time_world_instance.ball.all_data_dict["categoryID_and_channelID_2dList_of_create_Channel"] = [
-            [categoryID, f"{visitorUser_name}さん_作業場"],
-            [categoryID, f"{visitorUser_name}さん_休憩場"]
+            [categoryID, f"{visitorUser_name}さん_作業場", attach_ID=True],
+            [categoryID, f"{visitorUser_name}さん_休憩場", attach_ID=True]
         ]  # 作成するチャンネルを[カテゴリID,チャンネル名]
 
         # ランクをnew_userにアップグレード
